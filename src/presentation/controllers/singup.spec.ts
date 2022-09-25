4734a580a18e6468a857755a4752ffbfdd52d322
hgxkfad <buenomikhaelled@johndeere.com>
2022-09-25 12:31:58 -0300

    test: ensure singup controller return an error if no email is provider

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -15,4 +15,19 @@ describe('SingUP Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provider', () => {
+    const sut = new SingUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'anyPassword',
+        passwordConfirmation: 'anyPassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })

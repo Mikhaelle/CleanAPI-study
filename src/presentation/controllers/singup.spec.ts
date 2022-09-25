750d0a1a4e9b5c52b060e741e1a21a6c240aad6b
hgxkfad <buenomikhaelled@johndeere.com>
2022-09-25 12:17:45 -0300

    test: ensure SingUpController return 400 if no name is provider

---
+++ src/presentation/controllers/singup.spec.ts
@@ -0,0 +1,17 @@
+import { SingUpController } from './singup'
+
+describe('SingUP Controller', () => {
+  test('Should return 400 if no name is provider', () => {
+    const sut = new SingUpController()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'anyPassword',
+        passwordConfirmation: 'anyPassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
